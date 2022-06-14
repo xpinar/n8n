47613de8b1819294ae67d383cd5a4ddbb03e833e
xpinar <107260019+xpinar@users.noreply.github.com>
2022-06-14 09:35:38 +0200

    Update FriendGrid.node.ts

--- packages/nodes-base/nodes/FriendGrid/FriendGrid.node.ts
+++ packages/nodes-base/nodes/FriendGrid/FriendGrid.node.ts
@@ -1,18 +1,16 @@
 
-import {
-    IExecuteFunctions,
-} from 'n8n-core';
+import { IExecuteFunctions}   from 'n8n-core';
 
 import {
     IDataObject,
     INodeExecutionData,
     INodeType,
     INodeTypeDescription,
-} from 'n8n-workflow';
+}   from 'n8n-workflow';
 
 import {
     OptionsWithUri,
-} from 'request';
+}   from 'request';
 
 export class FriendGrid implements INodeType {
     description: INodeTypeDescription = {
